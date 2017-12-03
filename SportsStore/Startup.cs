b6b09a6bc4d76bc664e53c07089cee8e4666c245
philippbruhin <info@philippbruhin.ch>
2017-12-03 20:21:26 +0100

    pagination seo url

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -35,6 +35,12 @@ namespace SportsStore
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
+
+                routes.MapRoute(
+                    name: "pagination",
+                    template: "Products/Page{page}",
+                    defaults: new { Controller = "Product", action = "List" });
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
