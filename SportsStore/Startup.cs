16204bede6ee0ba636a243f9543157b96064222e
philippbruhin <info@philippbruhin.ch>
2017-12-09 14:00:21 +0100

    add to chart form and session for cart

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -25,6 +25,8 @@ namespace SportsStore
             services.AddTransient<IProductRepository, EFProductRepository>();
             //services.AddTransient <IProductRepository, FakeProductRepository>();
             services.AddMvc();
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +36,7 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvc(routes => {
 
                 routes.MapRoute(
